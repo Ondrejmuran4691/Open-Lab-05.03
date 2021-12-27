759e456906ff6fe3f2dd4609ed7861ee1f146faf
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-12-27 22:52:36 +0100

    puts , after thousands and milions and so on

---
+++ Open-Lab-05.03/Open-Lab-05.03/Numbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+namespace Open_Lab_05._03
+{
+    public class Numbers
+    {
+        public string FormatNum(int num)
+        {
+            string n = "";
+            string nu = "";
+            int counter = 0;
+            string final = "";
+            string final2 = "";
+            nu = num.ToString();
+            foreach(char c in nu.Reverse())
+            {
+                n += c;
+                counter++;
+                if(counter%3 == 0)
+                {
+                    n += ",";
+                }
+            }
+            final = n.ToString(); ;
+            foreach (char c in final.Reverse())
+            {
+                final2 += c;
+            }
+            if (final2.StartsWith(","))
+            {
+
+                final2 = final2.Remove(0, 1);
+            }
+            return final2.ToString();
+        }
+    }
+}
